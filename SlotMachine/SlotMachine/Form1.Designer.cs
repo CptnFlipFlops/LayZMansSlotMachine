5f1d5fa99dc8c1253a98d1501f9b4676adf8de8d
Sean <39991183+CptnFlipFlops@users.noreply.github.com>
2018-06-08 00:47:31 +0200

    Add files via upload

---
+++ SlotMachine/SlotMachine/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(41, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(134, 104);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(519, 327);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 35);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(201, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(134, 104);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(352, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(134, 104);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(41, 138);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(134, 104);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(201, 138);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(134, 104);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(352, 138);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(134, 104);
+            this.panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(41, 258);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(134, 104);
+            this.panel7.TabIndex = 2;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(201, 258);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(134, 104);
+            this.panel8.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(352, 258);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(134, 104);
+            this.panel9.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(594, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SlotMachine/SlotMachine/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int points = 500;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            label1.Text = "Points = 500";
+        }
+
+        public List<Int32> GetResults()
+        {
+            List<Int32> list1 = new List<int>();
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                list1.Add(rnd.Next(1, 10));
+                //  MessageBox.Show(list1[i].ToString());
+            }
+
+
+            return list1;
+
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            if (points < 0)
+            {
+                MessageBox.Show("YOU LOST!!!!!!!!!");
+            }
+            else
+            {
+
+            points -= 20;
+            }
+            label1.Text = "Points = " + points.ToString();
+            string s = "";
+            List<Int32> list = new List<Int32>();
+            list = GetResults();
+            int d = 0;
+            foreach (var item in list)
+            {
+
+                s = s + item;
+                if (d == 2 || d == 5)
+                {
+                    s = s + "\n";
+                }
+                d++;
+
+            }
+            //s = s + "========";
+
+            if (list[0] == list[1] && list[1] == list[2])
+            {
+                points = points + 50;
+            }
+            if (list[3] == list[4] && list[4] == list[5])
+            {
+                points = points + 100;
+            }
+            if (list[6] == list[7] && list[7] == list[8])
+            {
+                points = points + 50;
+            }
+
+
+            if (list[0] == list[3] && list[3] == list[6])
+            {
+                points = points + 50;
+            }
+            if (list[1] == list[4] && list[4] == list[7])
+            {
+                points = points + 100;
+            }
+            if (list[2] == list[5] && list[5] == list[8])
+            {
+                points = points + 50;
+            }
+
+            label1.Text = "Points = " + points.ToString();
+
+
+
+            panel1.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[0].ToString() + ".png");
+            panel2.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[1].ToString() + ".png");
+            panel3.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[2].ToString() + ".png");
+
+            panel4.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[3].ToString() + ".png");
+            panel5.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[4].ToString() + ".png");
+            panel6.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[5].ToString() + ".png");
+
+            panel7.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[6].ToString() + ".png");
+            panel8.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[7].ToString() + ".png");
+            panel9.BackgroundImage = Image.FromFile(@"C:\Users\Sean\Documents\Development\College\SlotMachine\" + list[8].ToString() + ".png");
+
+
+           // MessageBox.Show(s);
+        }
+    }
+}
